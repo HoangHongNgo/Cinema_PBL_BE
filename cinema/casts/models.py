4d2ddabf848867563c3023cbf94b8471642593d9
HoangHongNgo <honghoangngo@gmail.com>
2023-04-09 20:38:02 +0700

    add swagger

--- cinema/casts/models.py
+++ cinema/casts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cast(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    date_of_birth = models.DateField(auto_now_add=True)
+    genre = models.CharField(max_length=255)
--- cinema/showtimes/models.py
+++ cinema/showtimes/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from movies.models import Movie
-from cinemas.models import Cinema
+from cinemas.models import Cinema, Cinema_Room
 # Create your models here.
 
 
@@ -9,5 +9,7 @@ class Showtime(models.Model):
         Movie, on_delete=models.SET_NULL, blank=True, null=True)
     cinema = models.ForeignKey(
         Cinema, on_delete=models.SET_NULL, blank=True, null=True)
+    Cinema_Room = models.ForeignKey(
+        Cinema_Room, on_delete=models.SET_NULL, blank=True, null=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
