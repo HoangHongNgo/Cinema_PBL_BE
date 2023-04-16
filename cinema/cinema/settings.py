f0f5f186c0e4e6ae627206a4633f6962d4775701
HoangHongNgo <honghoangngo@gmail.com>
2023-04-16 21:08:41 +0700

    ready to deploy

--- cinema/cinema/settings.py
+++ cinema/cinema/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l#f78!bd+=hg1#@-_ltvg&_k5i%175q$ebp77opi-)1vo&4on@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.51']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
