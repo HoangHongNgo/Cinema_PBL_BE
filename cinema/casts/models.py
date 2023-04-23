2492653c8bc144d496c7eb6d7c6a08e76f30d6aa
HoangHongNgo <honghoangngo@gmail.com>
2023-04-23 18:10:52 +0700

    update movie api

--- cinema/casts/models.py
+++ cinema/casts/models.py
@@ -6,5 +6,6 @@ from django.db import models
 class Cast(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    image_path = models.TextField()
     date_of_birth = models.DateField(auto_now_add=True)
     genre = models.CharField(max_length=255)
