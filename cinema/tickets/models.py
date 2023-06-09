050d0bb32b2b1ba7fbc6df987766650fe0156869
Liem Nguyen <liem1762001@gmail.com>
2023-06-09 16:39:12 +0700

    create Payment

--- cinema/tickets/models.py
+++ cinema/tickets/models.py
@@ -5,12 +5,17 @@ from cinemas.models import *
 # Create your models here.
 
 
+class Payment(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.DO_NOTHING, related_name="payments", default='')
+    sale_time = models.DateTimeField(auto_now_add=True)
+
+
 class Ticket(models.Model):
     showtime = models.ForeignKey(
         Showtime, on_delete=models.SET_NULL, blank=True, null=True)
     seat_num = models.IntegerField()
     seat_row = models.CharField(max_length=2)
-    sale_date = models.DateTimeField(blank=True, null=True)
-    price = models.CharField(max_length=50)
-    owner = models.ForeignKey(
-        User, on_delete=models.SET_NULL, blank=True, null=True)
+    price = models.IntegerField(default=50)
+    payment = models.ForeignKey(
+        Payment, on_delete=models.SET_NULL, default=None, blank=True, null=True, related_name="tickets")
--- cinema/tickets/serializers.py
+++ cinema/tickets/serializers.py
@@ -5,6 +5,12 @@ from .models import *
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
-        fields = ['id', 'showtime', 'seat_num',
-                  'seat_row', 'sale_date', 'price', 'owner']
-        depth = 3
+        fields = "__all__"
+        depth = 2
+
+class PaymentSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer
+    class Meta:
+        model = Payment
+        fields = ['owner', 'tickets']
+        
